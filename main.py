80a7dccd89e073bf6a70423af6a56c63c5950d1a
brandongonzalez <brndn.gonz@gmail.com>
2022-09-22 00:23:20 -0700

    Added while loop to main file to continue converting messages if desired

--- main.py
+++ main.py
@@ -1,33 +1,40 @@
 from alphabet import alphabet, numbers, morse_code_alph, morse_code_nums
+from art import logo
 
 
 def to_morse_code(string):
     for char in string:
         if char in alphabet:
             position = alphabet.index(char)
-            string = string.replace(char, morse_code_alph[position] + ' ')
+            string = string.replace(char, morse_code_alph[position] + '   ')
         elif char in numbers:
             position = numbers.index(char)
-            string = string.replace(char, morse_code_nums[position] + ' ')
+            string = string.replace(char, morse_code_nums[position] + '   ')
 
     print(string)
 
 
-def to_alphabet(string):
-    for char in string:
-        if char in morse_code_alph:
-            position = morse_code_alph.index(char)
-            string = string.replace(char, alphabet[position] + ' ')
-        elif char in morse_code_nums:
-            position = morse_code_nums.index(char)
-            string = string.replace(char, numbers[position] + ' ')
+# def to_alphabet(msg):
+#     for word in msg:
+#         for char in word:
+#             if char in morse_code_alph:
+#                 position = morse_code_alph.index(char)
+#                 msg = msg.replace(char, alphabet[position])
+#             elif char in morse_code_nums:
+#                 position = morse_code_nums.index(char)
+#                 msg = msg.replace(char, numbers[position])
+#
+#     print(msg)
 
-    print(string)
-
-
+print(logo)
 print('Welcome to the Morse Code Generator!')
-direction = input("Type 'encode' to convert to Morse Code, type 'decode' to convert to English Alphabet:\n").lower()
 
+done = False
+while not done:
+    message = input("What is your message?\n").lower().replace(" ", "       ")
+    to_morse_code(message)
 
-message = input("What is your message?\n").lower().replace(" ", "   ")
-print(message)
+    restart = input("Type 'yes' if you want to convert more. Otherwise type 'no'.\n")
+    if restart == "no":
+        done = True
+        print("Thanks for using the Morse Code Generator!")
