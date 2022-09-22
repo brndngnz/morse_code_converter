2c0f10a814197f43803401318c8f2445a9b5e6b4
brandongonzalez <brndn.gonz@gmail.com>
2022-09-21 23:51:00 -0700

    Moved alphabet lists to alphabet.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from alphabet import alphabet, numbers, morse_code_alph, morse_code_nums
+
+
 def to_morse_code(string):
     for char in string:
         if char in alphabet:
@@ -21,16 +24,6 @@ def to_alphabet(string):
 
     print(string)
 
-
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-
-morse_code_alph = ['.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....', '..', '.---', '-.-', '.-..',
-                   '--', '-.', '---', '.--.', '--.-', '.-.', '...', '-', '..-', '...-', '.--', '-..-', '-.--', '--..']
-
-morse_code_nums = ['-----', '.----', '..---', '...--', '....-', '.....', '-....', '--...', '---..', '----.']
 
 print('Welcome to the Morse Code Generator!')
 direction = input("Type 'encode' to convert to Morse Code, type 'decode' to convert to English Alphabet:\n").lower()
